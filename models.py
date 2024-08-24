59f9a02bbc03011fe1263fd102a97b20052529d3
unknown <hoang0403ss@gmail.com>
2024-08-24 08:39:02 +0700

    init weights and use device

--- models.py
+++ models.py
@@ -88,3 +88,28 @@ class PatchDiscriminator(nn.Module):
     def forward(self, x):
         return self.model(x)
 
+def init_weight(model, init_mode = 'norm', gain = 0.02):
+    def init_func(m):
+        classname = m.__class__.__name__()
+        if hasattr(m, 'weight') and 'Conv' in classname:
+            if init_mode == 'norm':
+                nn.init.normal_(m.weight.data, 0.0, gain)
+            elif init_mode == 'xavier':
+                nn.init.xavier_normal_(m.weight.data, gain)
+            elif init_mode == 'kaiming':
+                nn.init.kaiming_normal_((m.weight.data, 0.0))
+            if hasattr(m, 'bias') and m.bias is not None:
+                nn.init.constant_(m.bias.data, 0.0)
+        elif 'BatchNorm' in classname:
+            nn.init.normal_(m.weight.data, 0.0, gain)
+            nn.init.constant_(m.bias.data, 0.0)
+
+    model.apply(init_func)
+    return model
+
+def init_model(model, device):
+    model = model.to(device)
+    model = init_weight(model)
+    return model
+
+
